29a95b1ae4c034cfcf706fce1b3f71409b3e02bb
HDRP <hdrp0720@gmail.com>
2023-07-19 15:43:46 +0900

    add scripts for custom terrain
    add editor gui table asset for project

---
+++ Assets/Scripts/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+public class CustomTerrainEditor : Editor
+{
+  private void OnEnable() 
+  {
+     
+  }
+
+  public override void OnInspectorGUI()
+  {
+
+  }
+}
